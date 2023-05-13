d9eda78e80292c24e4d8308cd57bdc41358ee039
Naman Singh Adhikari <nsadhikari21@gmail.com>
2023-05-13 21:05:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,21 +7,47 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
-}
-
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-}
-
-function addData() {
-  //Write your code here, just console.log
-}
-
-function removeFailedStudent() {
-  //Write your code here, just console.log
-}
-
-function concatenateArray() {
-  //Write your code here, just console.log
-}
+    //Write your code here , just console.log
+    arr.map(function(element){
+        if(element.marks>50){
+            console.log(element);
+        }
+    });
+  }
+  
+  function PrintStudentsbyForEach() {
+    //Write your code here , just console.log
+       arr.forEach(function(element){
+        if(element.marks>50){
+            console.log(element);
+        }
+       });
+  }
+  
+  function addData() {
+    //Write your code here, just console.log
+        let arr1 = {id:4,name:"susan",age:"20",marks:45};
+        arr.push(arr1);
+        console.log(arr);
+  }
+  
+  function removeFailedStudent() {
+    //Write your code here, just console.log
+    arr=arr.filter(function(element){
+        if(element.marks>50){
+          return element;
+        }
+    });
+    console.log(arr);
+  }
+  
+  function concatenateArray() {
+    //Write your code here, just console.log
+    let addStudents = [
+        {id:5,name:"Naman",age:"20",marks:55},
+        {id:6,name:"Aruj",age:"26",marks:100},
+        {id:7,name:"Vipin",age:"27",marks:10}
+      ];
+      arr=arr.concat(addStudents);
+      console.log(arr);
+  }
